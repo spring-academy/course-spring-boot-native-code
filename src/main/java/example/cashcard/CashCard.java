vmware-tanzu-learning/course-spring-brasb-build-a-rest-api-code

<simple-security-start> Set up simple security lab
- Add username to .sql files
- Add username to .json files
- Add username to CashCard and update references
- Update all tests with username reference as needed
- Add initial, commented-out SecurityConfig class

src/main/java/example/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package example.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-public record CashCard(@Id Long id, Double amount) {
+public record CashCard(@Id Long id, Double amount, String username) {
 }

src/test/java/example/cashcard/CashCardJsonTest.java
@@ -26,9 +26,9 @@ public class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00));
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1"));
     }
 
     @Test
@@ -48,11 +48,12 @@ public class CashCardJsonTest {
         String expected = """
                 {
                     "id": 99,
-                    "amount": 123.45
+                    "amount": 123.45, 
+                    "username": "sarah1"
                 }
                 """;
         assertThat(json.parse(expected))
-                .isEqualTo(new CashCard(99L, 123.45));
+                .isEqualTo(new CashCard(99L, 123.45, "sarah1"));
         assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
@@ -66,9 +67,10 @@ public class CashCardJsonTest {
     void cashCardListDeserializationTest() throws IOException {
         String expected = """
                 [
-                   { "id": 99, "amount": 123.45 },
-                   { "id": 100, "amount": 1.00 },
-                   { "id": 101, "amount": 150.00 }
+                     {"id": 99, "amount": 123.45 , "username": "sarah1"},
+                     {"id": 100, "amount": 1.00 , "username": "sarah1"},
+                     {"id": 101, "amount": 150.00, "username": "sarah1"}
+                                                  
                 ]
                 """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);

src/test/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (99, 123.45);
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (100, 1.00);
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (101, 150.00);
+INSERT INTO CASH_CARD(ID, AMOUNT, USERNAME) VALUES (99, 123.45, 'sarah1');
+INSERT INTO CASH_CARD(ID, AMOUNT, USERNAME) VALUES (100, 1.00, 'sarah1');
+INSERT INTO CASH_CARD(ID, AMOUNT, USERNAME) VALUES (101, 150.00, 'sarah1');

src/test/resources/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE cash_card
 (
-    ID     BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
-    AMOUNT NUMBER NOT NULL DEFAULT 0
+    ID       BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+    AMOUNT   NUMBER NOT NULL DEFAULT 0,
+    USERNAME VARCHAR(256) NOT NULL
 );

spring-academy/course-spring-boot-native-code

src/main/java/example/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package example.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-public record CashCard(@Id Long id, Double amount) {
+public record CashCard(@Id Long id, Double amount, String owner) {
 }

src/test/java/example/cashcard/CashCardJsonTest.java
@@ -26,9 +26,9 @@ public class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00));
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1"));
     }
 
     @Test
@@ -48,12 +48,13 @@ public class CashCardJsonTest {
         String expected = """
                 {
                     "id": 99,
-                    "amount": 123.45
+                    "amount": 123.45, 
+                    "owner": "sarah1"
                 }
                 """;
         assertThat(json.parse(expected))
-                .isEqualTo(new CashCard(99L, 123.45));
-        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+                .isEqualTo(new CashCard(99L, 123.45, "sarah1"));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 
@@ -66,9 +67,10 @@ public class CashCardJsonTest {
     void cashCardListDeserializationTest() throws IOException {
         String expected = """
                 [
-                   { "id": 99, "amount": 123.45 },
-                   { "id": 100, "amount": 1.00 },
-                   { "id": 101, "amount": 150.00 }
+                     {"id": 99, "amount": 123.45 , "owner": "sarah1"},
+                     {"id": 100, "amount": 1.00 , "owner": "sarah1"},
+                     {"id": 101, "amount": 150.00, "owner": "sarah1"}
+                                                  
                 ]
                 """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);

src/test/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (99, 123.45);
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (100, 1.00);
-INSERT INTO CASH_CARD(ID, AMOUNT) VALUES (101, 150.00);
+INSERT INTO CASH_CARD(ID, AMOUNT, OWNER) VALUES (99, 123.45, 'sarah1');
+INSERT INTO CASH_CARD(ID, AMOUNT, OWNER) VALUES (100, 1.00, 'sarah1');
+INSERT INTO CASH_CARD(ID, AMOUNT, OWNER) VALUES (101, 150.00, 'sarah1');

src/test/resources/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE cash_card
 (
-    ID     BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
-    AMOUNT NUMBER NOT NULL DEFAULT 0
+    ID       BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY,
+    AMOUNT   NUMBER NOT NULL DEFAULT 0,
+    OWNER    VARCHAR(256) NOT NULL
 );